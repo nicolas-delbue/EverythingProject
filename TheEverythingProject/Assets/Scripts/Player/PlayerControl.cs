caa4de300d7ed3d524847554f13b61a56ab16d45
Nick D <nicolas.k.delbue@gmail.com>
2026-03-13 21:26:00 -0400

    Rename and Camera Movement

---
+++ TheEverythingProject/Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    //Header Style
+    //Accessable 
+    //Serialized 
+    //Private 
+
+    [Header("Basic Movement")]
+    //Accessable Basic Movement
+    public float BaseMoveSpeed;
+    public float MaxSprintMoveSpeed;
+    //Private Basic Movement
+    private Vector3 moveDirection;
+    bool isSprinting = false;
+
+    [Header("Camera")]
+    //Accessable Camera Variables
+    public float sensitivity;
+    public float MaxLookAngle;
+    public float MinLookAngle;
+    //Private Camera Variables
+    private Vector3 lookDirection;
+    private float xRot;
+
+
+    /*[Header("Jumping")]
+    [Header("Crouch and Slide")]
+    [Header("Extra")]*/
+
+    [Header("Unity Attributes")]
+    //Accessable Attributes
+    //Serialized Attributes
+    [SerializeField]
+    private Rigidbody rb;
+    [SerializeField]
+    private GameObject playerCamera;
+    //Private Attributes
+    private PlayerInput inputActions;
+    private InputAction moveAction;
+    private InputAction lookAction;
+    private InputAction crouchAction;
+    private InputAction jumpAction;
+    private InputAction sprintAction;
+    
+
+    private void Awake()
+    {
+        //Inputs
+        inputActions = this.GetComponent<PlayerInput>();
+        moveAction = inputActions.actions["Move"];
+        lookAction = inputActions.actions["Look"];
+        crouchAction = inputActions.actions["Crouch"];
+        jumpAction = inputActions.actions["Jump"];
+        sprintAction = inputActions.actions["Sprint"];
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Start()
+    {
+        //Input Events
+
+        //Other Events
+
+        //Base Variables
+    }
+    void Update()
+    {
+        //Basic Movement
+        MovementUpdate();
+        //Look
+        CameraUpdate();
+        //Sprint Check
+        isSprinting = sprintAction.triggered;
+        Debug.Log(isSprinting);
+
+        //Jump Check
+        //inputActions.Player.Jump.triggered //Bool when you trigger the button
+        //Crouch Check
+
+        //Slide Check
+
+
+        //More Checks
+    }
+    private void FixedUpdate()
+    {
+        //Basic Movement
+        movePlayer(moveDirection);
+    }
+    private void MovementUpdate()
+    {
+        Vector2 move = moveAction.ReadValue<Vector2>();
+        moveDirection = transform.forward * move.y + transform.right * move.x;
+    }
+    private void movePlayer(Vector3 dir)
+    {
+        if(!isSprinting)
+        {
+            rb.linearVelocity = dir * BaseMoveSpeed * Time.fixedDeltaTime;
+        }
+        
+    }
+    private void CameraUpdate()
+    {
+        Vector2 look = lookAction.ReadValue<Vector2>();
+        xRot -= look.y * sensitivity * Time.deltaTime;
+        xRot = Mathf.Clamp(xRot, MinLookAngle, MaxLookAngle);
+        gameObject.transform.Rotate(0, look.x * sensitivity * Time.deltaTime, 0);
+        playerCamera.transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+    }
+}
